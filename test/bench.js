512aea2dc775e0e1cea981a582edadc7e03fa706
rhasson <rhasson@gmail.com>
2013-03-21 17:28:06 -0400

    added basic heap usage stats

--- test/bench.js
+++ test/bench.js
@@ -1,13 +1,14 @@
 var s = require('../index');
 var l = new s();
 
-var d,d1;
+var d,d1,m,m1;
 
-var doc = ['this is a test','another youtube test', 'maybe a vimeo video for fun', 'some other crap I dont care about'];
-var query = ['test', 'vimeo', 'video', 'youtube', 'fun', 'care'];
+var doc = ['this is a @vimeo test','another youtube test', 'http://vimeo.com/asfgaa', 'some other crap I dont care about'];
+var query = ['vimeo test', 'vimeo', 'video', 'http://youtube.com', 'fun', 'care'];
 var query2 = ['v', 'vi', 'vim', 'vimeo'];
 
 console.log('Start indexing');
+m = process.memoryUsage().heapUsed;
 d = new Date().getTime();
 
 for (var i=0; i < 10000; i++) {
@@ -15,18 +16,21 @@ for (var i=0; i < 10000; i++) {
 }
 
 d1 = new Date().getTime();
-console.log('End indexing - ', (d1 - d) / 1000, ' sec', '  index size: ', l._store.length);
+m1 = process.memoryUsage().heapUsed;
+console.log('End indexing - ', (d1 - d) / 1000, ' sec', '  index size: ', l._store.length, '  memory used: ', m1-m);
 
 console.log('Start searching');
+m = process.memoryUsage().heapUsed;
 d = new Date().getTime();
 
 for (var i=0; i < 10000; i++) {
 	query.forEach(function(q) {
-		l.search(q, function(){});
+		l.search(q, function(e,r){});
 	});
 }
 
 d1 = new Date().getTime();
-console.log('End searching - ', (d1 - d) / 1000, ' sec');
+m1 = process.memoryUsage().heapUsed;
+console.log('End searching - ', (d1 - d) / 1000, ' sec', '  memory used: ', m1-m);
 
-process.exit(1);
+process.exit(1);  //needed if the search test is very large and you don't want to wait for the callback to return
