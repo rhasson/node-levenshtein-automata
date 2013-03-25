929e4d12f79cfd0273df6a694e5cadf698a5269e
rhasson <rhasson@gmail.com>
2013-03-25 11:40:37 -0400

    cleaned a little

--- test/basic.js
+++ test/basic.js
@@ -1,11 +1,9 @@
 var lev = require('../index')
 	, store = lev.createMemoryStore()
-	, d
-	, d1
-	, doc = ['this is a @vimeo test','another youtube test', 'http://vimeo.com/asfgaa', 'some other crap I dont care about']
-	, doc2 = ['philly is a great city to vist', 'one of the nicest cities in the country', 'great tech scene too on vimeo']
-	, idx1
-	, idx2;
+	, d, d1, doc , doc2, idx1, idx2;
+
+doc = ['this is a @vimeo test','another youtube test', 'http://vimeo.com/asfgaa', 'some other crap I dont care about'];
+doc2 = ['philly is a great city to vist', 'one of the nicest cities in the country', 'great tech scene too on vimeo'];
 
 idx1 = lev.createIndex('test', {store: store});
 idx2 = lev.createIndex('test2', {store: store});
@@ -29,4 +27,4 @@ idx2
 		console.log('idx2: ', r);
 	});
 
-console.log(store)
+console.log('Memory store: ', store);
